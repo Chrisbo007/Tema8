062bcfefe406c448db00dc082963605a56206e8f
Chrisbo007 <56019736+Chrisbo007@users.noreply.github.com>
2020-04-23 22:39:47 +0200

    edit

---
+++ Oppgavesett/Oppgavesett2/Oppgave2/index.js
@@ -0,0 +1,11 @@
+/*
+
+Lag en moderne arrow-function av denne:
+
+function addMeToTheSong(name) {
+  console.log("Ground Control to Major " + name);
+} */
+
+
+const addMeToTheSong = (name) => console.log("Ground Control to Major " + name) 
+addMeToTheSong('Christina')
