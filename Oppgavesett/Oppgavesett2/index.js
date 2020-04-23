18db95f0edbcb7a4eab2422bd699c7654c2b6f11
Chrisbo007 <56019736+Chrisbo007@users.noreply.github.com>
2020-04-23 22:31:13 +0200

    Oppgavesett2-oppgave1

---
+++ Oppgavesett/Oppgavesett2/index.js
@@ -0,0 +1,13 @@
+/*
+Oppgaven: Lag funksjonen om til en arrow-funksjon: 
+
+function fortellEnVits() {
+  console.log("Ka sa den ene tørrfesken te den andre?");
+  console.log("long time no sea"); 
+}*/
+
+const fortellEnVits = () => {
+    console.log('Ka sa den ene tørrfesken til den andre?')
+    console.log('Long time no sea')
+}
+fortellEnVits()
