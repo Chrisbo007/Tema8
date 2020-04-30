2f94afeb2a779ca34790ca1200dc8f7f42aa5e73
Chrisbo007 <56019736+Chrisbo007@users.noreply.github.com>
2020-04-30 12:17:28 +0200

    Svelte

---
+++ svelte-boredapi/src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+	import Activity from './components/Activity.svelte'
+
+	let activity = {}
+	let show = false
+	const getActivity = () => {
+		show = false
+		fetch('https://www.boredapi.com/api/activity?type=recreational')
+			.then( res => res.json())
+				.then( json => {
+					activity = json
+					show = true
+					}  )
+}
+</script>
+
+<main>
+	<button on:click={getActivity}>Hit me</button>
+	
+	{#if show}
+		 <Activity {activity} />
+	{:else}
+		 <h2>Hit me</h2>
+	{/if}
+	
+
+</main>
+
+<style>
+main {
+	display: grid;
+	place-items: center;
+	height: 100vh;
+	position: relative;
+
+}
+
+button {
+	position: absolute;
+	top: 2rem;
+	background: black;
+	color: white;
+	cursor: pointer;
+	border-radius: 50%;
+	padding: 1rem
+}
+
+
+
+</style>
---
+++ svelte-boredapi/src/components/Activity.svelte
@@ -0,0 +1,28 @@
+	
+ <script>
+	import { scale } from 'svelte/transition'
+    export let activity
+ </script>   
+    
+    
+    
+    
+<div class="activity" in:scale>
+	<h2>{activity.activity}</h2>
+	<p>Type: {activity.type}</p>
+	<p>Accessibility: {activity.accessibility} </p>
+	<p>Price: {activity.price} </p>
+	<p>Participants: {activity.participants}</p>
+	<p>Link: {activity.link} </p>
+</div>
+
+
+<style>
+.activity {
+	width: 60vw;
+	height: 40vh;
+	padding: 2rem;
+	background: #eee;
+	box-shadow: 0 0 1rem 1px gray;
+}
+</style>
